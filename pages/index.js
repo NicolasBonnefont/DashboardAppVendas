4d9daff23aacab285506d4659e2de1f9f7f1b830
Nicolas <desenvolvimento@morinfo.com.br>
2021-01-12 20:50:00 -0300

    teste

--- pages/index.js
+++ pages/index.js
@@ -4,18 +4,18 @@ import Button from '@material-ui/core/Button';
 
 export default function Home() {
 
-  async function Logar() {
+/*   async function Logar() {
     event.preventDefault()
 
     console.log('TESTE')
   }
-
+ */
 
   return (
     <div className={styles.Container}>
 
       <div className={styles.CardLogin}>
-        <form className={styles.form} onSubmit={() => Logar()}>
+        <form className={styles.form} >
           <input className={styles.inputLogin} type="text" placeholder='Usuário...' />
           <input className={styles.inputLogin} type="text" placeholder='Senha...' />
           <Button className={styles.btnLogar} type='submit' variant="contained" color="primary">
